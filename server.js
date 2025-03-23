047be1a91382812e0d286c9ded0eda86baa41d6c
Vytor Oliveira <156971837+Vytor-Oliveira@users.noreply.github.com>
2025-03-23 23:01:29 +0000

    correção

--- server.js
+++ server.js
@@ -97,6 +97,3 @@ app.get('/produtos', async (req, res) => {
       res.status(500).json({ error: error.message });
   }
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
