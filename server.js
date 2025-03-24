708df89f8d09b82a9a3f86d6ba8e95e47f162ac3
Vytor Oliveira <156971837+Vytor-Oliveira@users.noreply.github.com>
2025-03-24 11:08:20 +0000

    atualização no crud

--- server.js
+++ server.js
@@ -95,5 +95,19 @@ app.get('/produtos', async (req, res) => {
   }
 });
 
+app.delete('/produtos/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+      const { error } = await supabase.from('produtos').delete().eq('id', id);
+      
+      if (error) throw error;
+
+      res.status(200).json({ message: 'Produto excluído com sucesso' });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+
 // Exporta o app para o Vercel
 module.exports = app;
